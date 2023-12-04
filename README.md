b60821aa561ad99c17536ca978c221232fa9ad73
NicoNeureiter <nico.neureiter@gmail.com>
2023-12-04 17:16:43 +0100

    added trace plot and download button

---
+++ README.md
@@ -0,0 +1,3 @@
+# sBayes interactive maps
+This project provides interactive maps to visualize spatial clusters inferred using the sBayes software. 
+
--- sbayes_dash/run.py
+++ sbayes_dash/run.py
@@ -1,15 +1,19 @@
 from __future__ import annotations
 
-import os; os.environ['USE_PYGEOS'] = '0'  # Fix for Pandas deprecation warning
+import os;os.environ['USE_PYGEOS'] = '0'  # Fix for Pandas deprecation warning
+
+from dash import dcc, no_update
+from dash.html import Figure
 
 from io import StringIO
 import base64
 from pathlib import Path
+import asyncio
 
 # from jupyter_dash import JupyterDash
 # from dash import Input, Output, State
 from dash import html
-from dash_extensions.enrich import Output, DashProxy, Input, MultiplexerTransform
+from dash_extensions.enrich import Output, DashProxy, Input, MultiplexerTransform, State
 import numpy as np
 import pandas as pd
 from numpy.typing import NDArray
@@ -17,8 +21,8 @@ from numpy.typing import NDArray
 from sbayes_dash.app_state import AppState
 from sbayes_dash import dash_components as components
 from sbayes_dash.load_data import Confounder, Objects
-from sbayes_dash.util import read_data_csv, reproject_locations
-from sbayes_dash.plotting import plot_summary_map, plot_sample_map, create_results_figure
+from sbayes_dash.util import read_data_csv, reproject_locations, COLOR_0, COLOR_1
+from sbayes_dash.plotting import plot_summary_map, plot_sample_map, create_data_figure
 
 # data_projection: str = "+proj=eqdc +lat_0=-32 +lon_0=-60 +lat_1=-5 +lat_2=-42 +x_0=0 +y_0=0 +ellps=aust_SA +units=m +no_defs "
 data_projection: str = "EPSG:4326"
@@ -31,10 +35,15 @@ def find_biggest_angle_gap(degrees: NDArray[float]) -> float:
     return (degrees[i + 1] + degrees[i]) / 2
 
 
-def parse_content(content: str):
-    content_type, content_bytestr = content.split(',')
-    content_str = str(base64.b64decode(content_bytestr))[2:-1]
-    return content_str.replace(r"\t", "\t").replace(r"\n", "\n")
+def parse_content(content: str) -> str:
+    if isinstance(content, str):
+        _, content = content.split(',')
+    content = str(base64.b64decode(content))[2:-1]
+    return content.replace(r"\t", "\t").replace(r"\n", "\n")
+
+
+def encode_content(content: bytes) -> bytes:
+    return base64.b64encode(content)
 
 
 def parse_clusters_samples(clusters_samples: str) -> NDArray[bool]:  # shape: (n_clusters, n_samples, n_sites)
@@ -47,17 +56,16 @@ def parse_clusters_samples(clusters_samples: str) -> NDArray[bool]:  # shape: (n
 
 
 # Initialized app
+# app = DashProxy(prevent_initial_callbacks='initial_duplicate', transforms=[MultiplexerTransform()], suppress_callback_exceptions=True)
 app = DashProxy(prevent_initial_callbacks=True, transforms=[MultiplexerTransform()], suppress_callback_exceptions=True)
 # app = JupyterDash(__name__, suppress_callback_exceptions=True)
 server = app.server
 state = AppState()
 
-# Set up the layout
-app.layout = components.get_base_layout()
 
 
 @app.callback(
-    #    Output('upload-data', 'disabled'),
+    Output('dashboard', 'children'),
     Output('upload-clusters', 'disabled'),
     Output('upload-data', 'children'),
     Input('upload-data', 'contents'),
@@ -76,6 +84,7 @@ def update_data(content, filename):
     state.locations = locations = reproject_locations(objects.locations, data_projection, "EPSG:4326")
     cut_longitude = find_biggest_angle_gap(locations[:, 0])
     locations[:, 0] = (locations[:, 0] - cut_longitude) % 360 + cut_longitude
+    n_objects = len(locations)
 
     family_names = np.array(families.group_names + [""])
     family_ids = []
@@ -90,17 +99,21 @@ def update_data(content, filename):
         "y": locations[:, 1],
         "name": objects.names,
         "family": family_names[family_ids],
-        "posterior_support": np.zeros(len(locations))
+        "cluster": np.zeros(n_objects, dtype=int),
+        "posterior_support": np.zeros(n_objects),
     })
 
-    # return True, False, html.Div([filename])
-    return False, html.Div([filename])
+    # Figure for the data plot:
+    state.data_fig = create_data_figure(state)
+
+    return components.build_tabs(state), False, html.Div([filename])
 
 
 @app.callback(
-    Output('uploaded-clusters', 'children'),
+    Output('results-tab', 'children'),
+    Output('results-tab', 'disabled'),
+    Output('tabs', 'value'),
     Output('upload-clusters', 'children'),
-    #    Output('upload-clusters', 'disabled'),
     Input('upload-clusters', 'contents'),
     Input('upload-clusters', 'filename'),
 )
@@ -110,46 +123,80 @@ def update_clusters(content, filename):
 
     state.clusters_path = Path(filename)
     clusters_str = parse_content(content)
-    state.clusters = clusters = parse_clusters_samples(clusters_str)
-    n_clusters, n_samples, n_sites = clusters.shape
-
-    fig = create_results_figure(state)
-
-    state.fig = fig
-    state.lines = fig.data[1:]
-    state.scatter = fig.data[0]
-
-    # Fix z-order so that lines are behind scatter:
-    fig.data = fig.data[::-1]
-
-    # for i in range(n_clusters):
-    #     f = fig.add_trace(
-    #         go.Scatter(x=[np.nan], y=[np.nan], legendgroup=f"Cluster {i}", marker_color=cluster_colors[i], name=f"Cluster {i}", visible="legendonly")
-    #     )
-    #
-    # fig.update_layout(showlegend=True)
+    state.clusters = parse_clusters_samples(clusters_str)
 
     results_components = components.build_results_components(state)
-    return results_components, html.Div([filename])  # , True
+    return results_components, False, "results-tab", html.Div([filename])
+
+
+@app.callback(
+    Output("data_map", "figure"),
+    Input("family-sizes", "hoverData"),
+)
+def hover_family(hover_data: dict):
+    family = hover_data["points"][0]["x"]
+    in_family = (state.object_data.family == family)
+    state.data_fig.update_traces(marker={
+        "color": np.where(in_family, COLOR_1, COLOR_0)
+    })
+    return state.data_fig
+
+
+@app.callback(
+    Output('data_map', 'figure'),
+    Input('feature-selector', 'value'),
+)
+def select_feature(feature: str):
+    # Which states can this feature take
+    states = state.data[feature].unique()
+    states = states[~pd.isna(states)]
+
+    # Create color palette for these states
+    color_seq = state.get_cluster_colors(len(states))
+    color_map = {s: color_seq[i] for i, s in enumerate(states)}
+    color_map[np.nan] = COLOR_0
+
+    # Color the map accordingly
+    colors = [color_map[x] for x in state.data[feature]]
+    state.data_fig.update_traces(marker={"color": colors})
+    return state.data_fig
 
 
 @app.callback(
     Output("map", "figure"),
     Input("i_sample", "value"),
 )
-def update_map(i_sample: int):
+def update_sample_map(i_sample: int):
     if state.clusters is None:
         return None
 
     state.i_sample = i_sample
-    return plot_sample_map(i_sample, state)
+    return plot_sample_map(state, i_sample)
+
+
+@app.callback(
+    Output("map", "figure"),
+    Output("i_sample", "value"),
+    Input("trace", "hoverData"),
+    State("summarize_switch", "on")
+)
+def hover_tracer(hover_data: dict, summarize: bool):
+    cluster = hover_data["points"][0]["curveNumber"]
+    state.highlighted_cluster = cluster
+    if summarize:
+        in_cluster = (state.object_data.cluster == cluster)
+        state.fig.update_traces(marker={"size": np.where(in_cluster, 10, 5)})
+        return state.fig, no_update
+    else:
+        sample = hover_data["points"][0]["x"]
+        return state.fig, sample
 
 
 @app.callback(
     Output("map", "figure"),
     Input("sample_range", "value"),
 )
-def update_map(sample_range: list[int]):
+def update_summary_map(sample_range: list[int]):
     if state.clusters is None:
         return None
 
@@ -159,32 +206,60 @@ def update_map(sample_range: list[int]):
 
 @app.callback(
     Output("map", "figure"),
-    Output("slider_div", "children"),
+    Output("slider_div", "style"),
+    Output("range_slider_div", "style"),
     Input("summarize_switch", "on"),
 )
-def switch_summarization(summarize: bool):
+def switch_summarization(summarize: bool) -> (Figure, dcc.Slider):
+    state.highlighted_cluster = None
+
+    sample_slider_style = {"width": "90%", "display": "none"}
+    range_slider_style = {"width": "90%", "display": "none"}
     if summarize:
-        state.slider = components.get_summary_range_slider(state)
-        slider_div = [
-            html.P(children="Sample range", style={"font-size": 14, "text-indent": "10px"}),
-            state.slider,
-        ]
         map_figure = plot_summary_map(state, [0, state.n_samples])
+        range_slider_style["display"] = "inline-block"
 
     else:
-        state.slider = components.get_sample_slider(state)
-        slider_div = [
-            html.P(children="Sample number", style={"font-size": 14, "text-indent": "10px"}),
-            state.slider,
-        ]
-        map_figure = plot_sample_map(state.i_sample, state)
+        map_figure = plot_sample_map(state, state.i_sample)
+        sample_slider_style["display"] = "inline-block"
 
-    return map_figure, slider_div
+    return map_figure, sample_slider_style, range_slider_style
 
 
-def main():
-    app.run_server(debug=True)
+@app.callback(
+    Output("download-map", "data"),
+    Input("download-map-button", "n_clicks"),
+    prevent_initial_call=True,
+)
+def download_figure(n_clicks):
+    map_html = state.serialize_results_map(filename="sBayes_map.html")
+    return map_html
+
+
+def main(port=8050, data_path: Path = None):
+    # if data_path:
+    #     data_path = Path(data_path)
+    #     with open(data_path, 'rb') as data_str:
+    #         data_encoded = encode_content(data_str.read())
+    #         # print(data_encoded)
+    #         update_data(data_encoded, data_path.name)
+
+    # Set up the layout
+    app.layout = components.get_base_layout()
+    app.run_server(debug=True, port=port)
 
 
 if __name__ == '__main__':
-    main()
+    import argparse
+    parser = argparse.ArgumentParser(description="Interactive visualisation of sBayes results.")
+    parser.add_argument("port", type=int, nargs="?", default=8050,
+                        help="The port  used to serve the application.")
+    parser.add_argument("-d", "--data", type=Path, nargs="?",
+                        help="Path to the data file")
+    cli_args = parser.parse_args()
+
+    # if cli_args.data:
+    #     asyncio.run(main(port=cli_args.port, data_path=cli_args.data))
+    #     print(cli_args.data)
+    # else:
+    main(port=cli_args.port, data_path=cli_args.data)
